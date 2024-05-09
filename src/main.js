59ebed479b3b6347a9b3060bd62549d3bef3eec7
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-05-09 21:26:16 +0800

    ElementPlus英文转中文

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import './assets/css/base.css'
-import './assets/css/iconfont.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import "./assets/css/base.css";
+import "./assets/css/iconfont.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 // 清除样式 pnpm i normalize.css -S
-import 'normalize.css/normalize.css'
-import App from './App.vue'
-import router from './router'
+import "normalize.css/normalize.css";
+import App from "./App.vue";
+import router from "./router";
+import locale from "element-plus/es/locale/lang/zh-cn";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(ElementPlus)
+app.use(createPinia());
+app.use(router);
+// ElementPlus英文转中文
+app.use(ElementPlus, { locale });
 
-app.mount('#app')
+app.mount("#app");
